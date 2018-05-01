bce602e11814ee74c41181c607430558f2f0a984
Dandiflower <>
2018-05-01 01:20:08 -0700

    javascript v4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,3 @@
-
-
-
-
 $(document).ready(function () {
     
     // variables
@@ -11,10 +7,10 @@ $(document).ready(function () {
     var computerNumber = 0;
     var userNumber = 0;
 
-    var card1 = 0;
-    var card2 = 0;
-    var card3 = 0;
-    var card4 = 0;
+    var card1 = [];
+    var card2 = [];
+    var card3 = [];
+    var card4 = [];
 
     var min = 19;
     var max = 120;
@@ -25,6 +21,34 @@ $(document).ready(function () {
         computerNumber = Math.floor(Math.random() * (max - min + 1)) + min;
         document.getElementById("computerNumber").innerHTML = computerNumber;
     }
+
+    function randomNumber1() {
+        var min = 1;
+        var max = 12;
+        card1 = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    randomNumber1();
+
+    function randomNumber2() {
+        var min = 1;
+        var max = 12;
+        card2 = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    randomNumber2();
+
+    function randomNumber3() {
+        var min = 1;
+        var max = 12;
+        card3 = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    randomNumber3();
+
+    function randomNumber4() {
+        var min = 1;
+        var max = 12;
+        card4 = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    randomNumber4();
 
     function updateTotal1() {
         document.getElementById("userNumber").innerHTML = userNumber;
@@ -48,79 +72,57 @@ $(document).ready(function () {
 
     function updateWins() {
         document.getElementById("wins").innerHTML = wins;
+        wins++;
+        alert("you won!");
     }
 
     function updateLosses() {
         document.getElementById("losses").innerHTML = losses;
+        losses++;
+        alert("you lost!");
     }
-
-    function randomNumber1() {
-        var min = 1;
-        var max = 12;
-        card1 = Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    randomNumber1();
-
-    function randomNumber2() {
-        var min = 1;
-        var max = 12;
-        card2 = Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    randomNumber2();
-
-    function randomNumber3() {
-        var min = 1;
-        var max = 12;
-        card3 = Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    randomNumber3();
-
-    function randomNumber4() {
-        var min = 1;
-        var max = 12;
-        card4 = Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    randomNumber4();
 
     // Main Section where functions are called upon...
 
+
     updateComputer();
-    
-    $(".btn1").on("click", function () {
+
+    $(".btn1").click(function () {
         updateTotal1();
-        console.log(card1);
         } 
     );
 
-    $(".btn2").on("click", function () {
+    $(".btn2").click(function () {
         updateTotal2();
-        console.log(card2);
         }
     );
 
-    $(".btn3").on("click", function () {
+    $(".btn3").click(function () {
         updateTotal3();
-        console.log(card3);
-    }
+        }
     );
 
-    $(".btn4").on("click", function () {
+    $(".btn4").click(function () {
         updateTotal4();
-        console.log(card4);
-    }
+        }
     );
+
+    if (userNumber > computerNumber) {
+        updateLosses();
+        updateComputer();
+        randomNumber1();
+        randomNumber2();
+        randomNumber3();
+        randomNumber4();
+    } 
+    
+    if (userNumber === computerNumber) {
+        updateWins();
+        updateComputer();
+        randomNumber1();
+        randomNumber2();
+        randomNumber3();
+        randomNumber4();
+    }
 
 });
-
-// <!-- which variables do we need -->
-
-//     <!-- button for clicking -->
-// $(#card1).click(function() {
-//     console.log(randomNumber());
-// });
-
-// <!-- random lotto number generator -->
