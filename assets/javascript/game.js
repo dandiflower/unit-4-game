5a21303c80be3c60a60989f741660a2a365859c5
Dandiflower <>
2018-04-30 23:58:02 -0700

    javascript v3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,9 +12,9 @@ $(document).ready(function () {
     var userNumber = 0;
 
     var card1 = 0;
-    // var card2 = [];
-    // var card3 = [];
-    // var card4 = [];
+    var card2 = 0;
+    var card3 = 0;
+    var card4 = 0;
 
     var min = 19;
     var max = 120;
@@ -31,10 +31,20 @@ $(document).ready(function () {
         userNumber = userNumber + card1;
     }
 
-    // function updateTotal2() {
-    //     document.getElementById("userNumber").innerHTML = userNumber;
-    //     userNumber = userNumber + card2;
-    // }
+    function updateTotal2() {
+        document.getElementById("userNumber").innerHTML = userNumber;
+        userNumber = userNumber + card2;
+    }
+
+    function updateTotal3() {
+        document.getElementById("userNumber").innerHTML = userNumber;
+        userNumber = userNumber + card3;
+    }
+
+    function updateTotal4() {
+        document.getElementById("userNumber").innerHTML = userNumber;
+        userNumber = userNumber + card4;
+    }
 
     function updateWins() {
         document.getElementById("wins").innerHTML = wins;
@@ -44,11 +54,37 @@ $(document).ready(function () {
         document.getElementById("losses").innerHTML = losses;
     }
 
-    function randomNumber() {
+    function randomNumber1() {
         var min = 1;
         var max = 12;
         card1 = Math.floor(Math.random() * (max - min + 1)) + min;
     }
+
+    randomNumber1();
+
+    function randomNumber2() {
+        var min = 1;
+        var max = 12;
+        card2 = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    randomNumber2();
+
+    function randomNumber3() {
+        var min = 1;
+        var max = 12;
+        card3 = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    randomNumber3();
+
+    function randomNumber4() {
+        var min = 1;
+        var max = 12;
+        card4 = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    randomNumber4();
 
     // Main Section where functions are called upon...
 
@@ -56,14 +92,27 @@ $(document).ready(function () {
     
     $(".btn1").on("click", function () {
         updateTotal1();
+        console.log(card1);
         } 
     );
 
-    // $(".btn1").on("click", function () {
-    //     updateTotal2();
-    //     }
-    // );
+    $(".btn2").on("click", function () {
+        updateTotal2();
+        console.log(card2);
+        }
+    );
 
+    $(".btn3").on("click", function () {
+        updateTotal3();
+        console.log(card3);
+    }
+    );
+
+    $(".btn4").on("click", function () {
+        updateTotal4();
+        console.log(card4);
+    }
+    );
 
 });
 
