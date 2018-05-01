5d0c97143fe0eb59b1ad8f355116811f82099337
Dandiflower <>
2018-04-30 23:23:26 -0700

    javascript v2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,68 @@
 
 
-// var wins = 0;
-// var losses = 0;
-// var computerNumber = 0;
-// var userNumber = 0;
 
-
-// var min = 19;
-// var max = 120;
-
-// // Function that update variables...
-
-
-// function updateComputer() {
-//     document.getElementById("computerNumber").innerHTML = computerNumber;
-// }
-
-// function updateTotal() {
-//     document.getElementById("userNumber").innerHTML = userNumber;
-// }
-
-// function updateWins() {
-//     document.getElementById("wins").innerHTML = wins;
-// }
-
-// function updateLosses() {
-//     document.getElementById("losses").innerHTML = losses;
-// }
-
-// function randomNumber() {
-//     computerNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
-// randomNumber();
 
 $(document).ready(function () {
     
-    
+    // variables
+
+    var wins = 0;
+    var losses = 0;
+    var computerNumber = 0;
+    var userNumber = 0;
+
+    var card1 = 0;
+    // var card2 = [];
+    // var card3 = [];
+    // var card4 = [];
+
+    var min = 19;
+    var max = 120;
+
+    // Function that update variables...
+
+    function updateComputer() {
+        computerNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        document.getElementById("computerNumber").innerHTML = computerNumber;
+    }
+
+    function updateTotal1() {
+        document.getElementById("userNumber").innerHTML = userNumber;
+        userNumber = userNumber + card1;
+    }
+
+    // function updateTotal2() {
+    //     document.getElementById("userNumber").innerHTML = userNumber;
+    //     userNumber = userNumber + card2;
+    // }
+
+    function updateWins() {
+        document.getElementById("wins").innerHTML = wins;
+    }
+
+    function updateLosses() {
+        document.getElementById("losses").innerHTML = losses;
+    }
+
+    function randomNumber() {
+        var min = 1;
+        var max = 12;
+        card1 = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    // Main Section where functions are called upon...
+
+    updateComputer();
     
     $(".btn1").on("click", function () {
-        console.log("hey");
+        updateTotal1();
         } 
     );
 
-
+    // $(".btn1").on("click", function () {
+    //     updateTotal2();
+    //     }
+    // );
 
 
 });
